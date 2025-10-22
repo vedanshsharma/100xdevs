4ee52ca2037cf0da1c153e799b339016e8ff3d30
Vedansh Sharma <svedansh99@gmail.com>
2025-10-22 13:24:57 +0530

    added db.js

--- course-selling-app/index.js
+++ course-selling-app/index.js
@@ -1,3 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import mongoose, { mongo } from "mongoose";
+import dotenv from 'dotenv';
+import { UserModel , TodoModel } from "./db.js";
+import auth from "./auth.js";
+import jwt from 'jsonwebtoken';
+import validate from "./validationMiddleware.js";
+import { signupSchema , createTodoSchema , todoUpdateSchema } from './validationSchemas.js';
+
+//import environment variables 
+dotenv.config();
+const connection_string = process.env.DB_CONNECTION_STRING;
+const port = process.env.PORT;
+const jwt_secret = process.env.JWT_SECRET;
+//setting localPath
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 

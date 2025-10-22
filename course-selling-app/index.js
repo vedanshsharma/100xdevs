0a8c5044063093c710dedd03e1f04a2463b329fe
Vedansh Sharma <svedansh99@gmail.com>
2025-10-22 14:19:18 +0530

    added app.listen at the end

--- course-selling-app/index.js
+++ course-selling-app/index.js
@@ -4,18 +4,32 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import mongoose, { mongo } from "mongoose";
 import dotenv from 'dotenv';
-import { UserModel , TodoModel } from "./db.js";
-import auth from "./auth.js";
+import connectDB from './models/db.js';
+// import auth from "./auth.js";
 import jwt from 'jsonwebtoken';
-import validate from "./validationMiddleware.js";
-import { signupSchema , createTodoSchema , todoUpdateSchema } from './validationSchemas.js';
+// import validate from "./validationMiddleware.js";
+// import { signupSchema , createTodoSchema , todoUpdateSchema } from './validationSchemas.js';
 
 //import environment variables 
 dotenv.config();
-const connection_string = process.env.DB_CONNECTION_STRING;
-const port = process.env.PORT;
-const jwt_secret = process.env.JWT_SECRET;
+const PORT = process.env.PORT;
+const JWT_SECRET = process.env.JWT_SECRET;
 //setting localPath
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+
+const app = express();
+app.use(express.json());
+
+
+//connect to DB and then set up server
+connectDB().then(() => {
+    app.listen(PORT , () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+}).catch(err => {
+    console.log("Failed to start server:", err);
+});
+
 

81e03e613b142e7a612837cd434442d1361577a4
Vedansh Sharma <svedansh99@gmail.com>
2025-10-24 01:48:49 +0530

    added signin for Admin route

--- course-selling-app/routes/adminRoutes.js
+++ course-selling-app/routes/adminRoutes.js
@@ -31,4 +31,38 @@ adminRouter.post('/signup' ,async (req , res) =>{
     }
 })
 
+adminRouter.post('/signin' ,async (req , res) => {
+    try{
+        const { email , password } = req.body;
+        const user = await AdminModel.findOne({
+            email 
+        });
+        if(!user){
+            return res.status(401).json({
+                "message" : "Invalid email or password"
+            })
+        }
+        const isMatch = await user.comparePassword(password);
+        if(isMatch){
+            const token = jwt.sign({
+                id : user._id.toString(),
+                role : user.role
+            } , jwt_secret , {
+                expiresIn : "1h"
+            });
+            res.status(200).json({
+                token : token
+            });
+        }
+        else{
+            res.status(403).json({
+                message : "Invalid email or password"
+            })
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
 export default adminRouter;
